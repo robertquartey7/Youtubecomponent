69afb1ab0d3ad245cb565a85ede58676788e1dc9
Robert Quartey <37972474+robertquartey7@users.noreply.github.com>
2022-12-03 22:00:20 -0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,75 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-import React from "react";
+import React from 'react'
+import './App.css'
 
-function App() {
-  const { isLoading, isError, data, error, isFetching } = useQuery({
-    queryKey: ["userName"],
-    queryFn: () => {
-      return axios.get("https://randomuser.me/api/");
-    },
-  });
 
-  if (isLoading) {
-    return <span>Loading</span>;
-  }
-
-  if (isError) {
-    return <span>Error: {error.message}</span>;
-  }
-
+function YoutubeComponent(props) {
   return (
-    <div>
-      <h1>sucess</h1>
+    <div className="container">
+      <img src={props.thumbnailImg}/>
+      <div className="content">
+        <p>{props.videoTitle}</p>
+        <p>{props.videoUploader}</p>
+        <p>{props.dateVideoUploaded}</p>
+        <p>{props.videoViews}</p>
+      </div>
     </div>
-  );
+
+  )
 }
 
-export default App;
+
+function App() {
+
+  let sampleData = [
+    {
+      thumbnailImg: "https://via.placeholder.com/600x400",
+      videoTitle: "He did WHAT?!?! (My intial reaction)",
+      videoUploader: "MaxTongster",
+      dateVideoUploaded: "10-31-2022",
+      videoViews: 10
+    },
+    {
+      thumbnailImg: "https://via.placeholder.com/600x400",
+      videoTitle: "I bought my best friend a Tesla (EMOTIONAL)",
+      videoUploader: "Dave Doughbrick",
+      dateVideoUploaded: "11-02-2022",
+      videoViews: 300000
+    },
+    {
+      thumbnailImg: "https://via.placeholder.com/600x400",
+      videoTitle: "React in 100 seconds",
+      videoUploader: "Fireship",
+      dateVideoUploaded: "2022-09-08",
+      videoViews: 255
+    }
+  ];
+
+
+  return (
+    <div className='app-container'>
+
+
+    
+      {
+        sampleData.map(element=>{
+          return  <YoutubeComponent
+           thumbnailImg={element.thumbnailImg} 
+           videoTitle={element.videoTitle}
+           videoUploader={element.videoUploader}
+           dateVideoUploaded={element.dateVideoUploaded}
+           videoViews={element.videoViews}
+        
+           
+           />
+        })
+      }
+      <YoutubeComponent/>
+      <YoutubeComponent/>
+      <YoutubeComponent/>
+      <YoutubeComponent/>
+    
+    </div>
+  )
+}
+
+export default App
